da0004bb1bbc57b0b42115233a2419451fd6eb77
ɐllɐ <molchanova.a.a@gmail.com>
2024-12-12 09:54:47 +0300

    task 16.3.1

---
+++ UserTicketService.Tests/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace UserTicketService.Tests
+{
+    public class UserRepository : IUserRepository
+    {
+        public IEnumerable<User> FindAll()
+        {
+            return null;
+        }
+    }
+
+    public interface IUserRepository
+    {
+        IEnumerable<User> FindAll();
+    }
+
+    public class User
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
+
